dc3bd292cfba75462aa6c5beaa5db72abcd995e2
Piotrek Frącek <piotrekfracek@gmail.com>
2018-01-12 18:06:04 +0100

    Remove leftover modules

--- src/modules/relations.js
+++ src/modules/relations.js
@@ -1,10 +1,5 @@
 import Session from './session/session.model'
 import User from './user/user.model'
-import Content from './content/content.model'
-import Language from './language/language.model'
 
 User.hasMany(Session)
 Session.belongsTo(User)
-
-Language.hasMany(Content, { foreignKey: 'languageSlug' })
-Content.belongsTo(Language, { foreignKey: 'languageSlug' })
