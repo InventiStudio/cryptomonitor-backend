cbf2632cbfe6effdef51097025e0529aabcbb362
Piotrek Frącek <piotrekfracek@gmail.com>
2018-01-12 22:12:17 +0100

    Update tests & lint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,4 +22,24 @@ module.exports = {
     quotes: [ 2, 'single' ],
     semi: ["error", "never"],
   },
+  overrides: {
+    files: ["*.spec.js", "test/**/*.js"],
+    env: {
+      mocha: true,
+    },
+    globals: {
+      server: true,
+      supertest: true,
+      expect: true,
+      clean: true,
+      spy: true,
+    },
+    rules: {
+      // Because of babel-plugin-rewire
+      'no-underscore-dangle': 0,
+      // It's common to use for-of with await inside for testing many conditions at once
+      'no-restricted-syntax': 0,
+      'no-await-in-loop': 0,
+    }
+  }
 }
